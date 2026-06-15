3750f5c7ed5b73fe8cf44ef7388fa12feea94dc7
NikosKatrakoulis <nikoskatrakoulis@gmail.com>
2026-06-15 12:45:07 +0300

    Complete todo app with add, delete and toggle

--- src/main/java/com/app/todoapp/controller/TaskController.java
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-//@RequestMapping("/tasks")
+@RequestMapping("/tasks")
 public class TaskController {
 
     private final TaskService taskService;
@@ -28,18 +28,18 @@ public class TaskController {
     @PostMapping
     public String createTask(@RequestParam String title){
         taskService.createTask(title);
-        return "redirect:/";
+        return "redirect:/tasks";
     }
 
     @GetMapping("/{id}/delete")
     public String deleteTask(@PathVariable Long id){
         taskService.deleTask(id);
-        return "redirect:/";
+        return "redirect:/tasks";
     }
 
     @GetMapping("/{id}/toggle")
     public String toggleTask(@PathVariable Long id){
         taskService.toggleTask(id);
-        return "redirect:/";
+        return "redirect:/tasks";
     }
 }
